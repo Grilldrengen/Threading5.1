22485874593c815727beb048179a621bffc1e3f2
Nikolaj Grill Nielsen <nikolaj@grillnielsen.dk>
2017-02-01 08:51:37 +0100

    Threads
    Thread

---
+++ Counters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading5
+{
+    public class Counters
+    {
+        public int Counter = 0;
+
+        public void addtwo()
+        {
+            Counter += 2;
+            Console.WriteLine("--> " + Counter);
+        }
+
+        public void subtractone()
+        {
+            Counter -= 1;
+            Console.WriteLine(Counter);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Threading5
+{
+    public class Program
+    {
+        static Counters c;
+
+        static object locker = new object();
+
+        static void ThreadMain1()
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+                c.addtwo();
+                Thread.Sleep(1000);
+                Monitor.Pulse(locker);
+                Monitor.Wait(locker);
+            }
+        }
+
+        static void ThreadMain2()
+        {
+            while (true)
+            {
+                Monitor.Enter(locker);
+                c.subtractone();
+                Monitor.Pulse(locker);
+                Monitor.Wait(locker);
+            }
+        }        
+
+        static void Main(string[] args)
+        {
+            c = new Counters();
+
+            Thread tid1 = new Thread(new ThreadStart(ThreadMain1));
+            Thread tid2 = new Thread(new ThreadStart(ThreadMain2));
+
+            tid1.Start();
+            tid2.Start();
+        }
+    }
+}
